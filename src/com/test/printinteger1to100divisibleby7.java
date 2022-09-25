0851f8ebf304276c66687dba1ccd1d8461e0a47f
Sudhakar-812 <113516794+Sudhakar-812@users.noreply.github.com>
2022-09-25 18:32:09 +0530

    Add files via upload

---
+++ src/com/test/printinteger1to100divisibleby7.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class printinteger1to100divisibleby7 {
+
+	public static void main(String[] args) {
+		
+		int i, n=100;
+		for(i=1;i<=n;i++)
+		{
+            if(i%7==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
