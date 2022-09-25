d28665d8bdc67d3e245095d5a2c590a0c301bc26
Sudhakar-812 <113516794+Sudhakar-812@users.noreply.github.com>
2022-09-25 18:27:14 +0530

    Add files via upload

---
+++ src/com/test/Break.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		int i , n=10;
+		for(i=0;i<=n;i++)
+		{
+		  if(i==5)
+		  {
+			break;
+		  }
+		System.out.println(i);
+		}
+	
+
+	}
+
+}
